be06c9edec0f55d25d8aaefb80f3b47aa14d70d3
GabbiJ <gabriellajanssen9@gmail.com>
2024-12-01 22:11:19 +0200

    all core functionality (CRUD) is working

--- _LevelUpAssessment/src/pages/ViewAllGraduates.jsx
+++ _LevelUpAssessment/src/pages/ViewAllGraduates.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import { useNavigate } from "react-router-dom";
+import 'reactjs-popup/dist/index.css';
 
 import '../App.css';
 import '../CreateGraduate.css';
+import Popup from "reactjs-popup";
 
 const ViewAllGraduates = () => {
 
@@ -36,34 +38,34 @@ const ViewAllGraduates = () => {
             <Navbar />
 
             <div style={{ marginLeft: "20px" }}>
-                    <table>
-                        <tr>
-                            <p>LEVEL UP 2024</p>
-                        </tr>
-                        <tr>
-                            <td style={{ whiteSpace: "nowrap" }}>
-                                <h1 className="heading"> GRADUATE LIST </h1>
-                            </td>
-                            <td style={{ width: "100%", paddingLeft: "75px" }}>
-                                <table className="table-lines">
-                                    <tr style={{ backgroundColor: "var(--blue)" }}>
-                                        <td>&nbsp;</td>
-                                    </tr>
-                                    <tr style={{ backgroundColor: "var(--green)" }}>
-                                        <td>&nbsp;</td>
-                                    </tr>
-                                    <tr style={{ backgroundColor: "var(--orange)" }}>
-                                        <td>&nbsp;</td>
-                                    </tr>
-                                    <tr style={{ backgroundColor: "var(--red)" }}>
-                                        <td>&nbsp;</td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
+                <table>
+                    <tr>
+                        <p>LEVEL UP 2024</p>
+                    </tr>
+                    <tr>
+                        <td style={{ whiteSpace: "nowrap" }}>
+                            <h1 className="heading"> GRADUATE LIST </h1>
+                        </td>
+                        <td style={{ width: "100%", paddingLeft: "75px" }}>
+                            <table className="table-lines">
+                                <tr style={{ backgroundColor: "var(--blue)" }}>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr style={{ backgroundColor: "var(--green)" }}>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr style={{ backgroundColor: "var(--orange)" }}>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr style={{ backgroundColor: "var(--red)" }}>
+                                    <td>&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
 
-                    </table>
-                </div>
+                </table>
+            </div>
 
             <section className="md:px-12 px-4 mt-6">
                 <table className="w-full border border-white md:rounded-t-xl rounded-t-lg overflow-hidden">
@@ -92,23 +94,73 @@ const ViewAllGraduates = () => {
                                 <td className="md:py-4 py-2 md:px-8 px-4"><strong>{grad.firstName}</strong> {grad.lastName}</td>
                                 <td className="md:py-4 py-2 md:px-8 px-4 md:block hidden">{grad.emailAddress}</td>
                                 <td className="md:py-4 py-2 md:px-8 px-4">
-                                    <button className="view-mode-button" onClick = {() => navigate(`/customer/${grad.graduateId}`)}>
+                                    <button className="view-mode-button" onClick={() => navigate(`/customer/${grad.graduateId}`)}>
                                         VIEW MODE
                                     </button>
-                                    <button className="update-button" onClick = {() => navigate(`/update/${grad.graduateId}`)}>
+                                    <button className="update-button" onClick={() => navigate(`/update/${grad.graduateId}`)}>
                                         UPDATE
                                     </button>
-                                    <button className="delete-button">
-                                        DELETE
-                                    </button>
+                                    <Popup trigger={<button className="delete-button">DELETE</button>} modal nested>
+                                        {
+                                            close => (
+                                                <div className='popup-delete'>
+                                                    <div>
+                                                        <label className="popup-label2" style={{ fontSize: "18pt", }}>DELETE GRADUATE</label>
+                                                        <label className="popup-label" style={{ fontSize: "24pt", fontWeight: "1000" }}>DELETE</label>
+                                                        <label className="popup-label" style={{ fontSize: "24pt" }}><strong>{grad.firstName}</strong> {grad.lastName}</label>
+                                                    </div>
+                                                    <br />
+                                                    <br />
+                                                    <div>
+                                                        <button className="delete-button" onClick={() => { deleteGrad(grad.graduateId, grad.firstName, grad.lastName, grad.emailAddress, grad.dateOfBirth); close(); }}>
+                                                            DELETE
+                                                        </button>
+                                                        <br />
+                                                        <button className="update-button" onClick={() => close()}>
+                                                            CANCEL
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                    </Popup>
                                 </td>
                             </tr>
-                        ))} 
+                        ))}
                     </tbody>
                 </table>
             </section>
         </div>
     )
+}
+
+const deleteGrad = async (id, firstName, lastName, emailAddress, dateOfBirth) => {
+    const grad = {
+        graduateId: id,
+        firstName: firstName,
+        lastName: lastName,
+        emailAddress: emailAddress,
+        dateOfBirth: dateOfBirth,
+    };
+
+    try {
+        const response = await fetch(`https://localhost:7041/api/Graduates/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(grad),
+        })
+
+        if (response.ok) {
+            alert('Graduate successfully deleted.')
+        } else {
+            alert('Graduate delete failed.')
+        }
+        
+    }catch (error) {
+        console.error('Error occured:', error);
+    }
 }
 
 export default ViewAllGraduates;
